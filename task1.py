a7529a134060084ab4b4277a6b79b7f28c145b92
erldas <daserlikov@gmail.com>
2020-07-16 20:10:01 +0600

    day3

---
+++ task1.py
@@ -0,0 +1,11 @@
+# list_ =[1, 2, 0.6, True, 'MAkers', 'Bishkek', 'ne nujen']
+
+# list_.extend([1, 2, 3, 4])
+# print(list_)
+# print(dir(list_))
+
+
+names = ['ali ', 'mike','paul']
+print(names[0])
+print(names[1])
+print(names[2])
